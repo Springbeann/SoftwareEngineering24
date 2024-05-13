c4197eb46a450e346d0f84bff7e28a1dd47f8882
Kamili Saintleger <77054888+Springbeann@users.noreply.github.com>
2024-05-13 15:18:31 +0200

    Update Patient.java

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -52,7 +52,7 @@ public class Patient {
      *         range
      */
     public List<PatientRecord> getRecords(long startTime, long endTime) {
-        // TODO Implement and test this method
+        // filtering patient records by start and end times
         List<PatientRecord> filteredRecords = new ArrayList<>();
         for (PatientRecord record : patientRecords) {
             if (record.getTimestamp() >= startTime && record.getTimestamp() <= endTime) {
