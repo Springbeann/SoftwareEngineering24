6f7f36a905cde536c496ce7ed7ad0a938165ea08
Kamili Saintleger <77054888+Springbeann@users.noreply.github.com>
2024-05-13 14:56:36 +0200

    Update TrendWindow.java

--- src/main/java/com/alerts/TrendWindow.java
+++ src/main/java/com/alerts/TrendWindow.java
@@ -1,2 +1,23 @@
 package com.alerts; 
 
+import java.util.ArrayList;
+
+public class TrendWindow {
+    private Patient patient; 
+
+    public TrendWindow(Patient patient){
+        this.patient = patient; 
+    }
+    public List<Double> getWindowValues(int patientID, long startTime, long endTime, String recordType) {
+        List<Double> windowValues = new ArrayList<>();
+        List<PatientRecord> records = patient.getRecords(startTime, endTime);
+        for (PatientRecord record : records) {
+            if (record.getRecordType().equals(recordType)) {
+                windowValues.add(record.getMeasurementValue());
+            }
+        }
+        return windowValues;
+    }
+
+    
+}
